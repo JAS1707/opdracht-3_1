ad76ce3a1f12c3af8e0522c3a68cd39c7d3819bd
MaximeG709lln <MaximeG709@LLN.CampusSintUrsula.be>
2026-05-29 09:25:26 +0200

    Charm + Pika implemented

--- Opdracht 3.1/Program.cs
+++ Opdracht 3.1/Program.cs
@@ -1,20 +1,34 @@
-﻿
-class Pokemon
+﻿class Pokemon
 {
     public string naam;
    public int levens  = 100;
-   public int aanvalsPunten = 5;
+   public int aanvalsPunten;
    public string type;
+   public string geluid;
    public void Geluid()
     {
-        Console.WriteLine($"{naam} maakt geluid");
+        Console.WriteLine($"{naam} maakt geluid: '{geluid}'");
     } 
-    class Charmander : Pokemon
+}
+
+class Charmander : Pokemon
     {
         public Charmander()
         {
             naam = "Charmander";
+            aanvalsPunten = 20;
             type = "Vuur";
+            geluid = "Charmander!";
         }
+    }
+
+class Pikachu : Pokemon
+{
+    public Pikachu()
+    {
+        naam = "Pikachu";
+        aanvalsPunten = 20;
+        type = "Elektriciteit";
+        geluid = "Pika!";
     }
 }
