b43b01fded65393a947761c0565551d86c7f5399
JAS1707 <jaspergoyvaerts5@gmail.com>
2026-05-29 09:21:00 +0200

    salamander

--- Opdracht 3.1/Program.cs
+++ Opdracht 3.1/Program.cs
@@ -1,12 +1,20 @@
-﻿class Pokemon
+﻿
+class Pokemon
 {
-    string naam = "";
-   int levens  = 100;
-   int aanvalsPunten = 5;
-   string type = "";
-
+    public string naam;
+   public int levens  = 100;
+   public int aanvalsPunten = 5;
+   public string type;
    public void Geluid()
     {
         Console.WriteLine($"{naam} maakt geluid");
     } 
+    class Charmander : Pokemon
+    {
+        public Charmander()
+        {
+            naam = "Charmander";
+            type = "Vuur";
+        }
+    }
 }
